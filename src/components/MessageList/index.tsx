0a56a8b990fcec2bdc8f9f5b83dc32fea0c1d1a6
Matheus Felipe Zanin <matt_z6@hotmail.com>
2021-10-25 10:54:39 -0300

    feat(MessageList): listen to new messages in realtime

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from 'react';
+import io from 'socket.io-client';
 
 import { nlwHeatApi } from '../../services/nlwHeatApi';
 
-import styles from './styles.module.scss';
-
 import logoImg from '../../assets/logo.svg';
+
+import styles from './styles.module.scss';
 
 type MessageUser = {
   id: string;
@@ -19,8 +20,30 @@ type Message = {
   user: MessageUser;
 }
 
+const socket = io('http://localhost:3333');
+
+const messagesQueue: Message[] = [];
+
+socket.on('new_message', message => {
+  messagesQueue.push(message);
+});
+
 export function MessageList() {
   const [messages, setMessages] = useState<Message[]>([]);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      if (messagesQueue.length > 0) {
+        setMessages(prevState => [messagesQueue[0], prevState[0], prevState[1]].filter(Boolean));
+
+        messagesQueue.shift();
+      }
+    }, 3_000);
+
+    return () => {
+      clearInterval(timer);
+    }
+  }, []);
 
   useEffect(() => {
     async function loadLastThreeMessages() {
